f15723fac73921cf7ccad4c033a7969846abdfcf
Ali Naddafi <131589857+alinaddafi@users.noreply.github.com>
2024-08-03 15:26:00 +0330

    Add files via upload

---
+++ tictactoe/tictactoe.py
@@ -0,0 +1,174 @@
+"""
+Tic Tac Toe Player
+"""
+import copy
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] != EMPTY:
+                count += 1
+
+    if board == initial_state():
+        return X
+    if count % 2 == 1:
+        return O
+    else:
+        return X
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    moves = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                moves.add((i, j))
+
+    return moves
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    if action not in actions(board):
+        raise Exception("Invalid Action!!!")
+
+    board2 = copy.deepcopy(board)
+    board2[action[0]][action[1]] = player(board)
+
+    return board2
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    columns = []
+    #checks rows
+    for row in board:
+        xcounter = row.count(X)
+        ocounter = row.count(O)
+        if xcounter == 3:
+            return X
+        if ocounter == 3:
+            return O
+
+    #checks columns
+    for j in range(len(board)):
+        column = [row[j] for row in board]
+        columns.append(column)
+
+    for j in columns:
+        xcounter = j.count(X)
+        ocounter = j.count(O)
+        if xcounter == 3:
+            return X
+        if ocounter == 3:
+            return O
+
+    #checks diagonally
+    if board[0][0] == O and board[1][1] == O and board[2][2] == O:
+        return O
+    if board[0][0] == X and board[1][1] == X and board[2][2] == X:
+        return X
+    if board[0][2] == O and board[1][1] == O and board[2][0] == O:
+        return O
+    if board[0][2] == X and board[1][1] == X and board[2][0] == X:
+        return X
+    #checks tie
+    return None
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    if winner(board) is not None:
+        return True
+    else:
+        for row in board:
+            if EMPTY in row:
+                return False
+        return True
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    if terminal(board):
+        return None
+    Max = float("-inf")
+    Min = float("inf")
+
+    if player(board) == X:
+        return Max_Value(board, Max, Min)[1]
+    else:
+        return Min_Value(board, Max, Min)[1]
+
+
+def Max_Value(board, Max, Min):
+    move = None
+    if terminal(board):
+        return [utility(board), None]
+    v = float('-inf')
+    for action in actions(board):
+        test = Min_Value(result(board, action), Max, Min)[0]
+        Max = max(Max, test)
+        if test > v:
+            v = test
+            move = action
+        if Max >= Min:
+            break
+    return [v, move]
+
+
+def Min_Value(board, Max, Min):
+    move = None
+    if terminal(board):
+        return [utility(board), None]
+    v = float('inf')
+    for action in actions(board):
+        test = Max_Value(result(board, action), Max, Min)[0]
+        Min = min(Min, test)
+        if test < v:
+            v = test
+            move = action
+        if Max >= Min:
+            break
+    return [v, move]
